74d3b2fa5ba714a08328f460ccce74e6bee88c5d
Laura Petit <102962237+Lautitep@users.noreply.github.com>
2023-10-06 16:52:00 +0200

    Add package.json in backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to my server');
+});
+
 app.post('/send-email', (req, res) => {
   const { name, email, subject, message } = req.body;
 
